0d53530b184673efdfb5455a685bc6a9264659a7
Kántor István <kantoristvan17@gmail.com>
2025-04-13 20:11:23 +0200

    todoservice-tests

--- frontend/todo-app/src/app/todo.service.spec.ts
+++ frontend/todo-app/src/app/todo.service.spec.ts
@@ -1,16 +1,67 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-import { TodoService } from './todo.service';
+import { TodoService, Todo } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TodoService],
+    });
+
     service = TestBed.inject(TodoService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get todos', () => {
+    const mockTodos: Todo[] = [
+      { id: 1, title: 'Test todo', completed: false, created_at: '2023-01-01' },
+    ];
+
+    service.getTodos().subscribe((todos) => {
+      expect(todos).toEqual(mockTodos);
+    });
+
+    const req = httpMock.expectOne('/api/todos');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTodos);
+  });
+
+  it('should add a todo', () => {
+    const newTodo: Partial<Todo> = {
+      title: 'New Todo',
+      description: 'Description',
+    };
+    const mockResponse: Todo = {
+      id: 1,
+      title: 'New Todo',
+      description: 'Description',
+      completed: false,
+      created_at: '2023-01-01',
+    };
+
+    service.addTodo(newTodo).subscribe((todo) => {
+      expect(todo).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('/api/todos');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newTodo);
+    req.flush(mockResponse);
   });
 });
