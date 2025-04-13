35718db57ac0fdeec45580b594f29c6d321104df
Kántor István <kantoristvan17@gmail.com>
2025-04-13 20:10:39 +0200

    theme-tests

--- frontend/todo-app/src/app/theme-service/theme-service.service.spec.ts
+++ frontend/todo-app/src/app/theme-service/theme-service.service.spec.ts
@@ -1,16 +1,53 @@
 import { TestBed } from '@angular/core/testing';
+import { ThemeService } from './theme-service.service';
+import { take, firstValueFrom } from 'rxjs';
 
-import { ThemeServiceService } from './theme-service.service';
-
-describe('ThemeServiceService', () => {
-  let service: ThemeServiceService;
+describe('ThemeService', () => {
+  let service: ThemeService;
 
   beforeEach(() => {
+    localStorage.clear();
+
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ThemeServiceService);
+    service = TestBed.inject(ThemeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should toggle theme', async () => {
+    const initialTheme = await firstValueFrom(
+      service.isDarkTheme$.pipe(take(1))
+    );
+
+    service.toggleTheme();
+
+    const newTheme = await firstValueFrom(service.isDarkTheme$.pipe(take(1)));
+
+    expect(newTheme).toBe(!initialTheme);
+    expect(localStorage.getItem('theme')).toBe(newTheme ? 'dark' : 'light');
+  });
+
+  it('should respect saved theme from localStorage', async () => {
+    localStorage.clear();
+    localStorage.setItem('theme', 'dark');
+
+    TestBed.resetTestingModule();
+    TestBed.configureTestingModule({});
+
+    service = TestBed.inject(ThemeService);
+    const darkTheme = await firstValueFrom(service.isDarkTheme$.pipe(take(1)));
+    expect(darkTheme).toBe(true);
+
+    localStorage.clear();
+    localStorage.setItem('theme', 'light');
+
+    TestBed.resetTestingModule();
+    TestBed.configureTestingModule({});
+
+    service = TestBed.inject(ThemeService);
+    const lightTheme = await firstValueFrom(service.isDarkTheme$.pipe(take(1)));
+    expect(lightTheme).toBe(false);
   });
 });
