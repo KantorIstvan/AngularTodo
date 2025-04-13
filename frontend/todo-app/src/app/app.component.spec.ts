867b4f5041c38ef82682608528bc30a7fc9fd822
Kántor István <kantoristvan17@gmail.com>
2025-04-13 20:06:57 +0200

    app-tests

--- frontend/todo-app/src/app/app.component.spec.ts
+++ frontend/todo-app/src/app/app.component.spec.ts
@@ -1,10 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { ThemeToggleComponent } from './theme-toggle/theme-toggle/theme-toggle.component';
+import { TodoService } from './todo.service';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    const mockTodoService = jasmine.createSpyObj('TodoService', [
+      'getTodos',
+      'addTodo',
+      'updateTodo',
+      'deleteTodo',
+    ]);
+    mockTodoService.getTodos.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule],
+      providers: [{ provide: TodoService, useValue: mockTodoService }],
     }).compileComponents();
   });
 
@@ -20,10 +34,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('todo-app');
   });
 
-  it('should render title', () => {
+  it('should render the todo list component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todo-app');
+    expect(compiled.querySelector('app-todo-list')).toBeTruthy();
   });
 });
