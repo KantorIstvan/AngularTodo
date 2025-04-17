060792f20e2a493c93ddbd9b1391e8ae8bf7cc46
Kántor István <kantoristvan17@gmail.com>
2025-04-17 12:51:36 +0200

    guard-and-interceptor

---
+++ frontend/todo-app/src/app/auth.interceptor.ts
@@ -0,0 +1,32 @@
+auth.interceptor.ts;
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = this.authService.getToken();
+
+    if (token) {
+      // Clone the request and add the auth header
+      const authRequest = request.clone({
+        headers: request.headers.set('x-auth-token', token),
+      });
+      return next.handle(authRequest);
+    }
+
+    return next.handle(request);
+  }
+}
