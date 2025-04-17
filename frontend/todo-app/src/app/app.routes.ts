d0d92dd9a53a49ccac8f8851c14604e385236ee0
Kántor István <kantoristvan17@gmail.com>
2025-04-17 13:54:43 +0200

    routes-fix

--- frontend/todo-app/src/app/app.routes.ts
+++ frontend/todo-app/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { AuthGuard } from './auth.guard';
+import { NoAuthGuard } from './noauth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'todos', pathMatch: 'full' },
   { path: 'todos', component: TodoListComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [NoAuthGuard] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [NoAuthGuard],
+  },
   { path: '**', redirectTo: 'todos' },
 ];
